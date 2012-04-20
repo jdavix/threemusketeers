3f1e44fdfd91f704c0f8e85b9193aa8e77fb6757
Jose Pedraza <pedrazaj_24@hotmail.com>
2012-04-19 22:06:46 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Threemusketeers::Application.config.secret_token = '16bd6080a20bfc842a439c3fe5353142f0cdb5a52eb8cafbdabc144b78fde2e8ed2ffe137714fe178e34e17b78ad049cc58386c765210883b9628c99e3eb254c'
